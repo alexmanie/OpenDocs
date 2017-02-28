d0479f9af7acb89d9fca09974ec663524663bd11
Alex Mañé <alex.martinez@raona.com>
2017-02-28 09:12:06 +0100

    commit

--- Droid/DocumentViewer_Droid.cs
+++ Droid/DocumentViewer_Droid.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Android.Content;
 using OpenDocs.Droid;
 using Xamarin.Forms;
@@ -10,7 +11,37 @@ namespace OpenDocs.Droid
 	{
 		public void ShowDocumentFile(string filepath, string mimeType)
 		{
-			var uri = Android.Net.Uri.Parse("file://" + filepath);
+			var filename = filepath;
+			string documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal); // Documents folder
+			var path = Path.Combine(documentsPath, filename);
+
+
+
+			// This is where we copy the file
+			Console.WriteLine(path);
+
+			//if (!File.Exists(path))
+			//{
+			//	File.Copy(filename, path);
+			//}
+
+			if (!File.Exists(path))
+			{
+				var s = Forms.Context.Resources.OpenRawResource(Resource.Raw.slides);  // RESOURCE NAME ###
+
+			    // create a write stream
+			    FileStream writeStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+			    // write to the stream
+			    ReadWriteStream(s, writeStream);
+			}
+
+			if (!File.Exists(path))
+			{
+				// ERROR
+			}
+
+			var uri = Android.Net.Uri.Parse("file://" + path);
+			//var uri = Android.Net.Uri.Parse("file://" + filepath);
 			var intent = new Intent(Intent.ActionView);
 			intent.SetDataAndType(uri, mimeType);
 			intent.SetFlags(ActivityFlags.ClearWhenTaskReset | ActivityFlags.NewTask);
@@ -23,6 +54,24 @@ namespace OpenDocs.Droid
 			{
 				//Let the user know when something went wrong
 			}
+		}
+
+		/// <summary>
+		/// helper method to get the database out of /raw/ and into the user filesystem
+		/// </summary>
+		void ReadWriteStream(Stream readStream, Stream writeStream)
+		{
+			int Length = 256;
+			Byte[] buffer = new Byte[Length];
+			int bytesRead = readStream.Read(buffer, 0, Length);
+			// write the required bytes
+			while (bytesRead > 0)
+			{
+				writeStream.Write(buffer, 0, bytesRead);
+				bytesRead = readStream.Read(buffer, 0, Length);
+			}
+			readStream.Close();
+			writeStream.Close();
 		}
 	}
 }
